7becb4bc0a992e7fa00fe011928cd085cfd6f2ee
carlosm2 <carlosm2@riseup.net>
2023-10-06 07:20:45 -0600

    Create alias.sh
    some random basic alias

---
+++ alias.sh
@@ -0,0 +1,20 @@
+# Alias variados
+
+# algunas cosas tomadas del perro: https://git.cuates.net/perro/aliases
+
+alias permisos='stat -c "Permiso %a | Creación %w | Modificación %y | %n" *'
+
+
+# expandir comandos
+
+alias mv='mv -v'
+alias rm='rm -v'
+alias free='free -h'
+alias diff='diff --color'
+
+alias alv='rm -rf'
+alias miip='curl https://ipapi.co/json'
+alias limpiaplis='sudo apt autoremove'
+alias df='df -h | grep -v snap'
+
+
